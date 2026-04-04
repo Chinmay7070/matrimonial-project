1f7632cfaf6e575c837a77c49aa1e47703aba1a7
Chinmay Chaudhari <chaudharichinmay227@gmail.com>
2026-04-04 15:09:04 +0530

    Initial project: backend + frontend

---
+++ backend/api-gateway (3)/api-gateway/src/test/java/com/matrimonial/gateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matrimonial.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
